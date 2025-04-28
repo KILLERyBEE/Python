d55caa9689ad8c3ebc975270b8ac416251614278
Avdhut Madhavi <71624737+KILLERyBEE@users.noreply.github.com>
2025-04-28 23:12:09 +0530

    c

--- PR21.py
+++ PR21.py
@@ -1,5 +1,11 @@
 import tkinter as tk
 root = tk.Tk()
-cb = tk.Checkbutton(root, text = "Click me" , command = root.destroy)
+root.title()
+root.size(400,400)
+cb = tk.Checkbutton(root, text = "Click me")
 cb.pack()
+rb = tk.Radiobutton(root,text = "Radio Button")
+rb.pack()
+b = tk.Button(root,text = "Button")
+b.pack()
 root.mainloop()
